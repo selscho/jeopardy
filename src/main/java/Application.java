968405d6b0f7e8f22ba86e3167ba7611dc8a1f57
Selina Schorer <sschorer@kabelmail.de>
2022-11-26 19:28:28 +0100

    show question panel

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,59 +4,71 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Application implements ActionListener {
+    CardLayout cardLayout = new CardLayout();
+    JPanel views = new JPanel(cardLayout);
+    final static String TABLE_OF_CATEGORIES = "table of categories";
+    final static String QUESTION = "question";
 
     public void run(){
         JFrame frame = new JFrame();
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        int numberOfCategories = 6;
-        frame.setLayout(new GridLayout(0, numberOfCategories));
+        JPanel tableOfCategories = new JPanel();
+        JPanel question = new JPanel();
 
-        frame.add(new JLabel("Category 1"));
-        frame.add(new JLabel("Category 2"));
-        frame.add(new JLabel("Category 3"));
-        frame.add(new JLabel("Category 4"));
-        frame.add(new JLabel("Category 5"));
-        frame.add(new JLabel("Category 6"));
+        views.add(tableOfCategories, TABLE_OF_CATEGORIES);
+        views.add(question, QUESTION);
+
+        frame.add(views);
+
+        int numberOfCategories = 6;
+        tableOfCategories.setLayout(new GridLayout(0, numberOfCategories));
+
+        tableOfCategories.add(new JLabel("Category 1"));
+        tableOfCategories.add(new JLabel("Category 2"));
+        tableOfCategories.add(new JLabel("Category 3"));
+        tableOfCategories.add(new JLabel("Category 4"));
+        tableOfCategories.add(new JLabel("Category 5"));
+        tableOfCategories.add(new JLabel("Category 6"));
 
         JButton b11 = new JButton("200");
         b11.addActionListener(this);
-        frame.add(b11);
+        tableOfCategories.add(b11);
 
-        frame.add(new JButton("200"));
-        frame.add(new JButton("200"));
-        frame.add(new JButton("200"));
-        frame.add(new JButton("200"));
-        frame.add(new JButton("200"));
+        tableOfCategories.add(new JButton("200"));
+        tableOfCategories.add(new JButton("200"));
+        tableOfCategories.add(new JButton("200"));
+        tableOfCategories.add(new JButton("200"));
+        tableOfCategories.add(new JButton("200"));
 
-        frame.add(new JButton("400"));
-        frame.add(new JButton("400"));
-        frame.add(new JButton("400"));
-        frame.add(new JButton("400"));
-        frame.add(new JButton("400"));
-        frame.add(new JButton("400"));
+        tableOfCategories.add(new JButton("400"));
+        tableOfCategories.add(new JButton("400"));
+        tableOfCategories.add(new JButton("400"));
+        tableOfCategories.add(new JButton("400"));
+        tableOfCategories.add(new JButton("400"));
+        tableOfCategories.add(new JButton("400"));
 
-        frame.add(new JButton("600"));
-        frame.add(new JButton("600"));
-        frame.add(new JButton("600"));
-        frame.add(new JButton("600"));
-        frame.add(new JButton("600"));
-        frame.add(new JButton("600"));
+        tableOfCategories.add(new JButton("600"));
+        tableOfCategories.add(new JButton("600"));
+        tableOfCategories.add(new JButton("600"));
+        tableOfCategories.add(new JButton("600"));
+        tableOfCategories.add(new JButton("600"));
+        tableOfCategories.add(new JButton("600"));
 
-        frame.add(new JButton("800"));
-        frame.add(new JButton("800"));
-        frame.add(new JButton("800"));
-        frame.add(new JButton("800"));
-        frame.add(new JButton("800"));
-        frame.add(new JButton("800"));
+        tableOfCategories.add(new JButton("800"));
+        tableOfCategories.add(new JButton("800"));
+        tableOfCategories.add(new JButton("800"));
+        tableOfCategories.add(new JButton("800"));
+        tableOfCategories.add(new JButton("800"));
+        tableOfCategories.add(new JButton("800"));
 
-        frame.add(new JButton("1000"));
-        frame.add(new JButton("1000"));
-        frame.add(new JButton("1000"));
-        frame.add(new JButton("1000"));
-        frame.add(new JButton("1000"));
-        frame.add(new JButton("1000"));
+        tableOfCategories.add(new JButton("1000"));
+        tableOfCategories.add(new JButton("1000"));
+        tableOfCategories.add(new JButton("1000"));
+        tableOfCategories.add(new JButton("1000"));
+        tableOfCategories.add(new JButton("1000"));
+        tableOfCategories.add(new JButton("1000"));
 
         frame.setVisible(true);
     }
@@ -65,5 +77,6 @@ public class Application implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         // TODO: cardlayout, um frage-fenster über grid legen zu können
+        cardLayout.show(views, QUESTION);
     }
 }
