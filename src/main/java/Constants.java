dcb15e3cb2d4a77d06d3b6ecba7b522e947a395f
Selina Schorer <sschorer@kabelmail.de>
2023-07-11 19:25:15 +0200

    Add README

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,6 +1,6 @@
 public class Constants {
     public static final String CATEGORY_1 = "Geschichte";
-    public static final String CATEGORY_2 = "Bayern";
+    public static final String CATEGORY_2 = "Reisen";
     public static final String CATEGORY_3 = "Logik";
     public static final String CATEGORY_4 = "Pop";
     public static final String CATEGORY_5 = "Unnütz";
